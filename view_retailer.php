ec6c0658092acbca1e42c2a6d7f2e62cba8f2eb8
Emon943 <motiur943@gmail.com>
2017-01-11 16:15:37 +0600

    fgbf

--- view_retailer.php
+++ view_retailer.php
@@ -1,37 +1,4 @@
- <?php $id=$_GET['sid'];
  
-    include 'config/config.php';
-	 $dbObj =	new DBUtility();
-	
-	$msg="Thank You For Being With Us";
-	if(isset($_POST['submit']))
-	{
-	$fname=$_POST['fname'];
-	$lname=$_POST['lname'];
-	$email=$_POST['email'];
-	$password=$_POST['password'];
-
-	$sql="INSERT INTO members_details(first_name,last_name,email_address,password)
-	VALUES ('".$fname."', '".$lname."', '".$email."','".$password."')";
-
-	if (mysql_query($sql)) {
-
-	} else {
-		echo "Error: " . $sql . "<br>" . mysql_error();
-	}
-	}
-	
-	
-	
-
-	
-
-
-
-
-    ?>
-	
-	
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 
